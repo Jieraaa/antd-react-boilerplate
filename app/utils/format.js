d74a475746dcfe9f476a995fe2e1465a39b3391a
yueer <zyue0119@163.com>
2018-01-05 10:21:20 +0800

    MOD:first modify

--- app/utils/format.js
+++ app/utils/format.js
@@ -5,7 +5,7 @@
 String.prototype.format=function()
 {
   if(arguments.length==0) return this;
-  for(let s=this, i=0; i<arguments.length; i++)
+  for(var s=this, i=0; i<arguments.length; i++)
     s=s.replace(new RegExp("\\{"+i+"\\}","g"), arguments[i]);
   return s;
 };
